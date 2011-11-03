e0a23441efb6e9e35a71a5483eda2faf8e5baf87
Julia West <juliamae@gmail.com>
2011-11-03 10:09:14 -0400

    Initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calico::Application.config.secret_token = 'e66ab2a7535174091533e455620d35bdc79be4b0f39cef7421417726fd1b00e8bcfe8b46bbe3bdeaf7550380307cae318842cc6b91897231381a0126bfda7edc'
