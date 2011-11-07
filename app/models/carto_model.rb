641de174094ea659d28cc3805cbd8f9a810374e3
Julia West <juliamae@gmail.com>
2011-11-07 11:33:21 -0500

    Creating basic CartoModel class to find and create records in cartodb utilizing ActiveModel conventions.

---
+++ app/models/carto_model.rb
@@ -0,0 +1,43 @@
+class CartoModel
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+  
+  attr_accessor :cartodb_id, :the_geom, :description, :name, :created_at, :updated_at, :lat, :lng
+  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+   
+  def self.find(id)
+    new( CartoDB::Connection.row table_name, id )
+  end
+  
+  def self.table_name
+    "#{Rails.application.class.parent.to_s}_#{Rails.env}_#{self.to_s.tableize}".downcase
+  end
+  
+  def save
+    if cartodb_id.present?
+      # todo
+    else
+      response = CartoDB::Connection.insert_row self.class.table_name, 
+          :the_geom => the_geom_to_s, :description => description, :name => name, :created_at => Time.now, :updated_at => Time.now
+      self.cartodb_id = response[:cartodb_id]
+      true
+    end
+  end
+  
+  def persisted?
+    false
+  end
+  
+  private
+  
+  def the_geom_to_s
+    raise "set lat and lng first" unless lat.present? && lng.present?
+    "SRID=4326;POINT(#{lng} #{lat})"
+  end
+end
