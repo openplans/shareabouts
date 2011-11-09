b4581a560a21f0413dd054207b70ca49d200e013
Julia West <juliamae@gmail.com>
2011-11-09 18:44:46 -0500

    Cleaning up map states; widgetfying shareabouts.js.

--- app/assets/javascripts/shareabouts.js
+++ app/assets/javascripts/shareabouts.js
@@ -1,232 +1,277 @@
 /*
- * requires leaflet, jquery 1.1.4, jakesgordon/javascript-state-machine, mustache, history.js
+ * requires leaflet, jquery 1.1.4, mustache, history.js
  */ 
-var Shareabouts = (function () {
+$.widget("ui.shareabout", (function() {
+  var map, // leaflet map
+      fsm, // state machine
+      newFeature, // marker for proposed feature
+      features, // object that stores map features by their ID
+      popup; // one popup on the map
     
-  var init = function (cfg) {
-    var map, // leaflet map
-        fsm, // state machin
-        newFeature, // marker for proposed feature
-        features = {},
-        popup    = new L.Popup({ offset: new L.Point(0,-33)}),
-        config   = { 
-          map : {
-            tileUrl            : null, 
-            center             : null,
-            // optional
-            mapElementId       : 'map', 
-            tileAttribution    : '', 
-            maxZoom            : 18,
-            initialZoom        : 13,
-            markerIcon         : null, // custom icon
-            newMarkerIcon      : null // custom icon for markers representing unsaved features
-          },
-          dataUrl              : null,
-          featurePopupTemplate : null, 
-          ajaxSubmitCfg : { // ajax config for new features
-            type : 'POST',
-            success : function(data) {
-              fsm.confirmSubmission(data);
-            }
-          }, 
-          callbacks : {} // state machine callbacks
+  return {
+    options : {
+      map : {
+        tileUrl            : null, 
+        center             : null,
+        // optional
+        tileAttribution    : '', 
+        maxZoom            : 18,
+        initialZoom        : 13,
+        markerIcon         : null, // custom icon
+        newMarkerIcon      : null // custom icon for markers representing unsaved features
+      },
+      dataUrl              : null,
+      featurePopupTemplate : null, 
+      callbacks : {} // state machine callbacks
+    },
+  
+    /**
+     * Constructor
+     */
+    _create : function() {
+      features = {};
+      popup    = new L.Popup({ offset: new L.Point(0,-33)});
+      map      = new L.Map( this.element.attr("id") );
+
+      map.setView(this.options.map.center, this.options.map.initialZoom);
+
+      map.addLayer(new L.TileLayer( this.options.map.tileUrl, {
+        maxZoom: this.options.map.maxZoom, attribution: this.options.map.tileAttribution
+      }));
+
+      map.on('layerremove', function(e){
+        // When the popup is removed (closed), reset the map state
+        if (e.layer == popup && fsm.can("reset")) fsm.reset();
+      });
+    
+      this._init_states();
+    
+      History.Adapter.bind(window,'statechange',function(){
+        var State = History.getState();
+        if (State.data.featureId) {
+          var featureLayer = features[State.data.featureId];
+          if (featureLayer._html) openPopupFor( featureLayer, featureLayer._html);
         }
-
-    $.extend(true, config, cfg);
-
-    // Initializing map 
-    map = new L.Map( config.map.mapElementId );
+      });
+    },
     
-    map.setView(config.map.center, config.map.initialZoom);
+    /*****************
+      PUBLIC
+    *****************/
     
-    map.addLayer(new L.TileLayer( config.map.tileUrl, {
-      maxZoom: config.map.maxZoom, attribution: config.map.tileAttribution
-    }));
+    /**
+     * Drops a pin on the map - at latLng, if provided, or map center. 
+     * Advances map state to locatingFeature. 
+     * Can be called from ready state.
+     * @param {L.LatLng} latLng the optional location to place the locating marker.
+     */
+    locateNewFeature : function(latLng) {
+      fsm.locateNewFeature(latLng);
+    },
     
-    map.on('layerremove', function(e){
-      // When the popup is removed (closed), reset the map state
-      if (e.layer == popup && fsm.can("reset")) fsm.reset();
-    });
-        
+    /**
+     * Gets the form via the ajax options passes in. 
+     * Unless otherwise specified via success callback, loads form into popup for newFeature marker.
+     * @param {Object} ajaxOptions options for jQuery.ajax(). By default, success loads responseData.view into popup.
+     */
+    loadNewFeatureForm : function(ajaxOptions) {
+      fsm.loadNewFeatureForm(ajaxOptions);
+    },
+    
+    finalizeNewFeature : function() {
+      fsm.finalizeNewFeature();
+    },
+    
+    submitNewFeature : function(ajaxOptions) {
+      fsm.submitNewFeature(ajaxOptions);
+    },
+      
     /**
      * Returns the leaflet map
      */
-    this.getMap = function () { return map; };
-    
+    getMap : function () { return map; },
+  
     /**
      * Returns the state machine object
      */
-    this.state = function () { return fsm; };
+    state : function () { return fsm; },
     
+    /**
+     * Returns the new Feature marker
+     */
+    getNewFeatureMarker : function () { return newFeature },
+  
     /**
      * Opens the popup for a feature
      */
-    this.viewFeature = function(fId, view) {
-      openPopupFor( features[fId], view ? view : featureLayer._html);
-    };
-    
-    // State machine handles the flow of creating and viewing map features
-    fsm = StateMachine.create({
-      initial : 'empty',
-      events  : [
-        { name: 'loadFeatures',      from: ['empty', 'ready'],  to: 'ready' },
-        { name: 'locateFeature',     from: 'ready',             to: 'locatingFeature' },
-        { name: 'finalizeFeature',   from: 'locatingFeature',   to: 'finalizingFeature' },
-        { name: 'submitFeature',     from: 'finalizingFeature', to: 'submittingFeature' },
-        { name: 'confirmSubmission', from: 'submittingFeature', to: 'confirmingSubmission' },
-        { name: 'viewFeature',       from: 'ready',             to: 'viewingFeature' },
-        { name: 'reset', from: ['locatingFeature', 'finalizingFeature', 'confirmingSubmission', 'viewingFeature'], to: 'ready' }
-      ], 
-      callbacks : config.callbacks
-    });
-
-    fsm.onchangestate = function(eventName, from, to) { 
-      console.log("Transitioning from " + from + " to " + to + " via " + eventName);
-    };
-    
-    /*
-     * Expects a geoJSON object or an array of geoJSON features whose properties contain a unique ID called 'id'
-     */
-    fsm.onloadFeatures = function (eventName, from, to, dataUrl) {
-      if (!dataUrl) return;
-      
-      $.getJSON(dataUrl, function(data){
-        var geojsonLayer = new L.GeoJSON(null, {
-          // Assumes all features are points ATM
-          pointToLayer : function(latlng) {
-            var markerOpts = {};
-            if ( config.map.markerIcon ) markerOpts.icon = new config.map.markerIcon();
-            return new L.Marker(latlng, markerOpts);
-          }
-        });
-        
-        // Triggered as features are individually parsed
-        geojsonLayer.on('featureparse', function(featureparse) {
-          setupMarker(featureparse.layer, featureparse.properties);
-        });
-        
-        if (typeof data == "object") data = data.features;
-        $.each(data, function(i,f) { geojsonLayer.addGeoJSON(f); });
-        map.addLayer(geojsonLayer);
-      });      
-    }
-    
-    /*
-     * Drops a marker on the map at the latlng, if provided, or in the middle
-     */
-    fsm.onenterlocatingFeature = function (eventName, from, to, latlng) {      
-      if (!latlng) latlng = map.getCenter();
-      var markerOpts = { draggable : true };
-      if ( config.map.newMarkerIcon ) markerOpts.icon = new config.map.newMarkerIcon();
-        
-      newFeature = new L.Marker(latlng, markerOpts);
-      map.addLayer(newFeature);
-    };
-    
-    /*
-     * Performs an ajax request. 
-     * If the json response contains a view property, that will be displayed in the marker popup.
-     */
-    fsm.onleavelocatingFeature = function (eventName, from, to, options) {
-      newFeature.dragging.disable();
-      if ( typeof options == "object" ){
-        $.extend(true, options, { 
-          type : 'GET', 
-          success: function(data){
-            if (data.view) {
-              openPopupFor(newFeature, $("<div>").html($("<div class='shareabouts submit'>").html(data.view)).html());
-              $("div.shareabouts.submit form").submit(function(e){
-                e.preventDefault();
-                                
-                var ajaxOptions = { 
-                  data : $(this).serialize() + "&lat=" + newFeature.getLatLng().lat + "&lng=" + newFeature.getLatLng().lng, 
-                  url : $(this).attr("action") 
-                };
-                
-                $.extend(true, ajaxOptions, config.ajaxSubmitCfg);
-
-                fsm.submitFeature( ajaxOptions );
-              });
-            }
-            fsm.transition();
-          },
-          dataType : 'json'
-        });
-        $.ajax(options);
-      }
-      return false;
-    };
-    
-    /*
-     * Submits new feature form. 
-     * Wait until response to transition to confirmingFeature state.
-     */
-    fsm.onentersubmittingFeature = function (eventName, from, to, ajaxCfg) {
-      $.ajax(ajaxCfg);
-    };
-    
-    /*
-     * Creates a marker for the new feature using the properties in responseData.geoJSON. 
-     */
-    fsm.onenterconfirmingSubmission = function (eventName, from, to, responseData) {
-      var markerOpts = { };
-      if ( config.map.markerIcon ) markerOpts.icon = new config.map.markerIcon();
-      
-      var marker = new L.Marker(newFeature.getLatLng(), markerOpts);
-      
-      setupMarker(marker, responseData.geoJSON.properties);
-      
-      map.removeLayer(newFeature);
-      map.addLayer(marker);
-      
-      // Update history state 
-      var fId = responseData.geoJSON.properties.id;
-      History.pushState( { featureId : fId }, "Feature " + fId, "?feature=" + fId);   
-    };
-    
-    /*
-     * Removes all the layers related to feature submission. 
-     */
-    fsm.onreset = function (eventName, from, to) {
-      newFeature.closePopup();
-      map.removeLayer(newFeature);
-    };
-    
-    // Load initial data
-    fsm.loadFeatures(config.dataUrl);
-    
-    History.Adapter.bind(window,'statechange',function(){
-      var State = History.getState();
-      if (State.data.featureId) {
-        var featureLayer = features[State.data.featureId];
-        if (featureLayer._html) openPopupFor( featureLayer, featureLayer._html);
-      }
-    });
-    
-    var openPopupFor = function(layer, content) {
+    viewFeature : function(fId, view) {
+      this.openPopupFor( features[fId], view ? view : featureLayer._html);
+    },
+  
+    // fixing ...
+    openPopupFor : function(layer, content) {
       popup.setContent(content);
       popup.setLatLng(layer.getLatLng());
       if (!popup._opened) map.openPopup(popup);
-    };
-    
-    var setupMarker = function(marker, properties) {
-      if (config.featurePopupTemplate)
-        marker._html = $.mustache( config.featurePopupTemplate, properties );
+    },
+  
+    _setupMarker : function(marker, properties) {
+      if (this.options.featurePopupTemplate)
+        marker._html = $.mustache( this.options.featurePopupTemplate, properties );
 
       var fId = properties.id;
-      
-      marker._id = fId;
-      marker.on("click", markerClick);
-      
-      features[fId] = marker;
-    };
     
-    var markerClick = function(click) {
+      marker._id = fId;
+      marker.on("click", this._markerClick);
+    
+      features[fId] = marker;
+    },
+  
+    _markerClick : function(click) {
       // Change history state on feature click
       var fId = this._id;
       History.pushState( { featureId : fId }, "Feature " + fId, "?feature=" + fId);   
-    };
-  };
+    },
+  
+    /*
+     * Private
+     */
+    _init_states : function() {
+      var shareabout = this;
+      
+      // State machine handles the flow of creating and viewing map features
+      fsm = StateMachine.create({
+        initial : 'ready',
+        events  : [
+          // if geoJSON object or data url in options
+          { name: 'loadFeatures', from: 'ready', to: 'loadingFeatures'},
+          // UI triggered. drops a marker onto the map. if passed L.LatLng, drops marker there.
+          { name: 'locateNewFeature', from: 'ready', to: 'locatingNewFeature'},
+          // UI triggered. can move from ready to loadingNewFeatureForm without first locating if location not required, chosen later, etc
+          // if feature is already located, pass geoJSON representation of feature to loadNewFeatureForm
+          { name: 'loadNewFeatureForm', from: ['ready', 'locatingNewFeature'], to: 'loadingNewFeatureForm'},
+          // called internally after form has been loaded
+          { name: 'finalizeNewFeature', from: 'loadingNewFeatureForm', to: 'finalizingNewFeature'},
+          // UI triggered. pass geoJSON representation to transition and callback on success
+          { name: 'submitNewFeature', from: ['finalizingNewFeature', 'locatingNewFeature'], to: 'submittingNewFeature'},
 
-  return init;
-})();
+          // Ways to get back to ready
+          { name: 'ready', from : ['loadingFeatures', 'submittingNewFeature'], to: 'ready'},
+          { name: 'cancel', from: ['locatingNewFeature', 'finalizingNewFeature'], to: 'ready'}
+        ], 
+        callbacks : shareabout.options.callbacks
+      });
+    
+      fsm.onchangestate = function(eventName, from, to) { 
+        console.log("Transitioning from " + from + " to " + to + " via " + eventName);
+      };
+
+      /*
+       * Expects a geoJSON object or an array of geoJSON features whose properties contain a unique ID called 'id'
+       */
+      fsm.onloadFeatures = function (eventName, from, to, dataUrl) {
+        if (!dataUrl) return;
+
+        $.getJSON(dataUrl, function(data){
+          var geojsonLayer = new L.GeoJSON(null, {
+            // Assumes all features are points ATM
+            pointToLayer : function(latlng) {
+              var markerOpts = {};
+              if ( shareabout.options.map.markerIcon ) markerOpts.icon = new shareabout.options.map.markerIcon();
+              return new L.Marker(latlng, markerOpts);
+            }
+          });
+
+          // Triggered as features are individually parsed
+          geojsonLayer.on('featureparse', function(featureparse) {
+            shareabout._setupMarker(featureparse.layer, featureparse.properties);
+          });
+
+          if (typeof data == "object") data = data.features;
+          $.each(data, function(i,f) { geojsonLayer.addGeoJSON(f); });
+          map.addLayer(geojsonLayer);
+          fsm.ready();
+        });      
+      }
+
+      /*
+       * Drops a marker on the map at the latlng, if provided, or in the middle
+       */
+      fsm.onlocateNewFeature = function (eventName, from, to, latlng) {   
+        if (!latlng) latlng = map.getCenter();
+        
+        var markerOpts = { draggable : true };
+        if ( shareabout.options.map.newMarkerIcon ) markerOpts.icon = new shareabout.options.map.newMarkerIcon();
+
+        newFeature = new L.Marker(latlng, markerOpts);
+        map.addLayer(newFeature);
+      };
+      
+      /*
+       * Performs an ajax request. 
+       * By default, if the json response contains a view property, that will be displayed in the marker popup.
+       */
+      fsm.onloadNewFeatureForm = function (eventName, from, to, ajaxOptions) {
+        newFeature.dragging.disable();
+        
+        var ajaxCfg = { 
+          type : 'GET', 
+          success: function(data){
+            if (data.view) {
+              shareabout.openPopupFor(newFeature, $("<div>").html($("<div class='shareabouts submit'>").html(data.view)).html());
+            }
+            shareabout.finalizeNewFeature();
+          },
+          dataType : 'json'
+        };
+        
+        if ( typeof ajaxOptions == "object" ) $.extend(true, ajaxCfg, ajaxOptions);
+        $.ajax(ajaxCfg);
+      };
+      
+      /*
+       * Submits new feature form. 
+       */
+       fsm.onsubmitNewFeature = function (eventName, from, to, ajaxOptions) {
+         var ajaxCfg = {
+           type : 'POST',
+           success : function(data) {
+             fsm.ready(data);
+           }
+         };
+         if ( typeof ajaxOptions == "object" ) $.extend(true, ajaxCfg, ajaxOptions);
+         $.ajax(ajaxCfg);
+      };
+
+      /*
+       * Creates a marker for the new feature using the properties in responseData.geoJSON. 
+       */
+      fsm.onleaveconfirmingSubmission = function (eventName, from, to, responseData) {
+        var markerOpts = { };
+        if ( shareabout.options.map.markerIcon ) markerOpts.icon = new shareabout.options.map.markerIcon();
+
+        var marker = new L.Marker(newFeature.getLatLng(), markerOpts);
+
+        shareabout._setupMarker(marker, responseData.geoJSON.properties);
+
+        map.removeLayer(newFeature);
+        map.addLayer(marker);
+
+        // Update history state 
+        var fId = responseData.geoJSON.properties.id;
+        History.pushState( { featureId : fId }, "Feature " + fId, "?feature=" + fId);   
+      };
+
+      /*
+       * Removes all the layers related to feature submission. 
+       */
+      fsm.onreset = function (eventName, from, to) {
+        newFeature.closePopup();
+        map.removeLayer(newFeature);
+      };
+
+      // Load initial data
+      fsm.loadFeatures(this.options.dataUrl);      
+    }
+  }; // end widget function return
+})());
