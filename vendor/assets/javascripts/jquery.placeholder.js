af14db853dd8069977f9b19bd57b63e20a1e386e
Julia West <juliamae@gmail.com>
2011-12-01 14:07:48 -0500

    Popup content matches wireframes; Light styling within popup; Utilizing description field of feature point instead of creating a comment as the description.

---
+++ vendor/assets/javascripts/jquery.placeholder.js
@@ -0,0 +1,15 @@
+// http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+
+$('[placeholder]').focus(function() {
+  var input = $(this);
+  if (input.val() == input.attr('placeholder')) {
+    input.val('');
+    input.removeClass('placeholder');
+  }
+}).blur(function() {
+  var input = $(this);
+  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+    input.addClass('placeholder');
+    input.val(input.attr('placeholder'));
+  }
+}).blur();
