d4de9a0b722bf431b97083866c1b3c347c2f2206
Julia West <juliamae@gmail.com>
2012-01-05 11:17:52 -0500

    Explicitly setting UTC time and accounting for UTC offset in news controller.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,7 +3,7 @@ class NewsController < ApplicationController
   def index
     respond_to do |format|
       format.json do
-        since  = params[:utc] ? Time.at( params[:utc].to_i/1000 ) : Date.today - 1000.years
+        since  = params[:utc] ? Time.at( params[:utc].to_i/1000 - Time.now.utc_offset ) : Date.today - 1000.years
         @items = FeaturePoint.where("created_at > '#{since.to_formatted_s(:psql)}'")
           .limit(params[:limit])
           .order('created_at desc')
