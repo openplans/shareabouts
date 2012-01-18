5f1e3022219549eeb6fcb3c7baba0eeb2b086ec1
Julia West <juliamae@gmail.com>
2012-01-18 12:28:31 -0500

    Fixing bug in CommentsController that was neglecting to associate comment with logged in user.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,12 @@ class CommentsController < ApplicationController
   before_filter :get_commentable, :only => :create
   
   def create
-    # explicit commentable stuff due to lack of AR
-    @vote = Comment.create params[:comment].merge( {:commentable_id => @commentable.id, :commentable_type => commentable_class } )
+    @comment = @commentable.comments.create  params[:comment].merge(:user => current_user)    
     
     respond_to do |format|
       format.json { 
         render :json => {
-          :vote => @vote.as_json, 
+          :comment => @comment.as_json, 
           :view => render_to_string(:partial => "#{commentable_class.tableize}/show.html", :locals => { commentable_class.underscore.to_sym => @commentable }) 
         }
       }
