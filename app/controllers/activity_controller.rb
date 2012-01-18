89d260e8428c4eeb73edfedc37d7c526b6991020
Julia West <juliamae@gmail.com>
2012-01-18 09:59:39 -0500

    Pulling activity items for the ticker via ID and not created at; Adding infinite scroll to ticker.

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -3,8 +3,15 @@ class ActivityController < ApplicationController
   def index
     respond_to do |format|
       format.json do
-        since  = params[:utc] ? Time.at( params[:utc].to_i/1000 - Time.now.utc_offset ) : Date.today - 1000.years
-        @activity_items = ActivityItem.where("created_at > '#{since.to_formatted_s(:psql)}'")
+        where = if params[:after].present?
+          ["id > ?", params[:after]]
+        elsif params[:before].present?
+          ["id < ?", params[:before]]
+        else
+          ""
+        end
+        
+        @activity_items = ActivityItem.where(where)
           .limit(params[:limit])
           .order('created_at desc')
         render "index.html"
