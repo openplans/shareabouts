9dbf2ba1d8f0a7d2714507b89ff9795373bd5be8
Julia West <juliamae@gmail.com>
2012-01-24 16:41:18 -0500

    Adding existing feature points to region after region create.

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,11 +1,15 @@
 class Region < ActiveRecord::Base
   
+  has_many   :feature_regions
+  has_many   :feature_points, :through => :feature_regions, :source => :feature, :source_type => 'FeaturePoint'
   belongs_to :shapefile, :inverse_of => :regions
   
   validates :shapefile, :presence => true
   validates :the_geom, :presence => true
   
   serialize :metadata
+  
+  after_create :add_feature_points_to_region
   
   def display_name
     "#{kind} - #{name}"
@@ -21,6 +25,21 @@ class Region < ActiveRecord::Base
   
   def default?
     shapefile.default?
+  end
+  
+  private
+  
+  def add_feature_points_to_region    
+    feature_point_ids = ActiveRecord::Base.connection.execute <<-SQL
+      select feature_points.id 
+      FROM   feature_points, regions 
+      WHERE  regions.id = #{id} 
+      AND    ST_CONTAINS(regions.the_geom, feature_points.the_geom)
+    SQL
+    
+    feature_point_ids.each do |row|
+      feature_regions.create :feature_id => row["id"].to_i, :feature_type => "FeaturePoint"
+    end
   end
   
 end
