4bcb19183771cfb9e9b4139391b09524308bd393
Julia West <juliamae@gmail.com>
2012-01-30 13:56:37 -0500

    Adding first admin to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,6 @@
 for option_name in SiteOption::Names
   SiteOption.find_or_create_by_option_name option_name
 end
+
+# Create initial admin user
+Admin.create :email => "admin@example.com", :password => "p@ssw0rd", :password_confirmation => "p@ssw0rd", :level => 100
