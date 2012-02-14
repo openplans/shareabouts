96757c514988e3a10a5dd79c7b382690062d194a
Julia West <juliamae@gmail.com>
2012-02-14 11:41:26 -0500

    Replacing ajax-loader.gif with spinner.js.

---
+++ vendor/assets/javascripts/jquery.spin.js
@@ -0,0 +1,15 @@
+$.fn.spin = function(opts) {
+  this.each(function() {
+    var $this = $(this),
+        data = $this.data();
+
+    if (data.spinner) {
+      data.spinner.stop();
+      delete data.spinner;
+    }
+    if (opts !== false) {
+      data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+    }
+  });
+  return this;
+};
