4a5aee3abb6b0ae4dbfccb8b12e76bb9690e9adc
Julia West <juliamae@gmail.com>
2012-03-27 14:14:40 -0400

    Destroying contributions associated with a profile on profile delete.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,11 @@
 class Profile < ActiveRecord::Base
   
-  belongs_to :user
-  has_many :activity_items
-  has_many :comments
-  has_many :feature_points
-  has_many :feature_polygons
-  has_many :votes
+  belongs_to :user, :dependent => :destroy
+  has_many :activity_items, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
+  has_many :feature_points, :dependent => :destroy
+  has_many :feature_polygons, :dependent => :destroy
+  has_many :votes, :dependent => :destroy
   
   validates :user_agent, :uniqueness => { :scope => :client_ip }, :allow_blank => true
   validates :user_id, :uniqueness => true, :allow_blank => true
