2c70048de2bfa488369fcaf7f9a0b86de74e47d1
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-07-17 19:38:43 -0400

    Create the template_postgis database AS A TEMPLATE for the CI server

--- ci/install.sh
+++ ci/install.sh
@@ -13,6 +13,7 @@ sudo pip install -r requirements.txt
 
 # Create a PostGIS template database
 psql -c "CREATE DATABASE template_postgis;" -U postgres
+psql -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';" -U postgres
 createlang plpgsql template_postgis -U postgres
 psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql -q
 psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql -q
