4c1f6efb860b310c8295f704fc3018fc77d3f864
Aaron Ogle <atogle@gmail.com>
2012-07-17 15:06:47 -0400

    add simple view

--- src/sa_web/urls.py
+++ src/sa_web/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from . import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,5 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^', include('sa_web.urls')),
+    url(r'^$', views.index, name='index'),
 )
