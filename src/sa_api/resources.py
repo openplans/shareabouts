eee495c3c44cef60a55097aac608826ea51236b1
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-07-20 17:16:47 -0400

    Get rid of any keys we don't know how to deal with in the place resource

--- src/sa_api/resources.py
+++ src/sa_api/resources.py
@@ -13,6 +13,19 @@ class PlaceResource (resources.ModelResource):
         }
 
     def validate_request(self, origdata, files=None):
-        data = origdata.copy()
-        data['location'] = utils.to_wkt(origdata.get('location'))
+        if origdata:
+            data = origdata.copy()
+
+            # For now, ignore fields we don't know how to deal with.
+            known_fields = set(self.model._meta.get_all_field_names())
+            for key in origdata:
+                if key not in known_fields:
+                    del data[key]
+
+            # Convert the location into something that GeoDjango knows how to
+            # deal with.
+            data['location'] = utils.to_wkt(origdata.get('location'))
+
+        else:
+            data = origdata
         return super(PlaceResource, self).validate_request(data, files)
