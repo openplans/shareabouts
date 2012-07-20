c0b2563c2bda42b5bd8f5ed61cf6f7a33f424019
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-07-20 18:39:22 -0400

    Re-enable CSRF protection, and handle it on the front-end

--- src/sa_web/static/sa/js/models.js
+++ src/sa_web/static/sa/js/models.js
@@ -6,3 +6,60 @@ var Shareabouts = Shareabouts || {};
   });
 
 })(Shareabouts, jQuery);
+
+
+/*****************************************************************************
+
+CSRF Validation
+---------------
+Django protects against Cross Site Request Forgeries (CSRF) by default. This
+type of attack occurs when a malicious Web site contains a link, a form button
+or some javascript that is intended to perform some action on your Web site,
+using the credentials of a logged-in user who visits the malicious site in their
+browser.
+
+Since the API proxy view sends requests that write data to the Shareabouts
+service authenticated as the owner of this dataset, we want to protect the API
+view against CSRF. In order to ensure that AJAX POST/PUT/DELETE requests that
+are made via jQuery will not be caught by the CSRF protection, we use the
+following code. For more information, see:
+https://docs.djangoproject.com/en/1.4/ref/contrib/csrf/
+
+*/
+
+jQuery(document).ajaxSend(function(event, xhr, settings) {
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    function sameOrigin(url) {
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    function safeMethod(method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+});
--- src/sa_web/views.py
+++ src/sa_web/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import ensure_csrf_cookie
 from proxy.views import proxy_view
 
 
+@ensure_csrf_cookie
 def index(request):
     return render(request, 'index.html')
 
 
-# TODO Should this really be exempt?
-@csrf_exempt
 def api(request, path):
     url = settings.SHAREABOUTS_API_ROOT + path
     return proxy_view(request, url)
