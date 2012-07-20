4fb5cf57d5b6bac8716e5a77d2c9f0548f48c481
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-07-20 16:43:30 -0400

    Only use basic authentication in api views

--- src/sa_api/views.py
+++ src/sa_api/views.py
@@ -1,9 +1,11 @@
-from djangorestframework import views
+from djangorestframework import views, authentication
 from . import resources
 
 class PlaceCollectionView (views.ListOrCreateModelView):
     # TODO: Decide whether pagination is appropriate/necessary.
     resource = resources.PlaceResource
+    authentication = (authentication.BasicAuthentication,)
 
 class PlaceInstanceView (views.InstanceModelView):
     resource = resources.PlaceResource
+    authentication = (authentication.BasicAuthentication,)
