71def8252d823d831a7c5bf707056b01e5bb8f97
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-07-24 12:53:42 -0400

    Bootstrap initial place data into the template

--- src/sa_web/views.py
+++ src/sa_web/views.py
@@ -1,4 +1,7 @@
+import requests
+
 from django.shortcuts import render
+from django.template import RequestContext
 from django.conf import settings
 from django.views.decorators.csrf import ensure_csrf_cookie
 from proxy.views import proxy_view
@@ -6,7 +9,21 @@ from proxy.views import proxy_view
 
 @ensure_csrf_cookie
 def index(request):
-    return render(request, 'index.html')
+    # Bootstrapping initial data.
+    apiroot = settings.SHAREABOUTS_API_ROOT
+
+    places_res = requests.get(apiroot + 'places/',
+                              headers={'Accepts':'application/json'})
+    if places_res.status_code == 200:
+        places_json = places_res.text
+    else:
+        import pdb; pdb.set_trace()
+        places_json = u'[]'
+
+    context = {
+        'places_json': places_json
+    }
+    return render(request, 'index.html', context)
 
 
 def api(request, path):
