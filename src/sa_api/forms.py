a30be445611e54254132e3d606eeb9c38db13c98
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-07-25 05:10:39 -0400

    Get the activity form to handle full ISO datetimes

--- src/sa_api/forms.py
+++ src/sa_api/forms.py
@@ -1,8 +1,19 @@
 from django import forms
+from dateutil.parser import parse
 
-ISO8601_FORMAT = ['%Y-%m-%dT%H:%M:%SZ',
-                  '%Y-%m-%dT%H:%M:%S']
+class IsoDateTimeField (forms.DateTimeField):
+    def to_python(self, value):
+        try:
+            return super(IsoDateTimeField, self).to_python(value)
+        except forms.ValidationError, e:
+            # Last ditch, use dateutil
+            try:
+                return parse(value)
+            except:
+                raise e
+
+
 class ActivityForm (forms.Form):
-    before = forms.DateTimeField(required=False, input_formats=ISO8601_FORMAT)
-    after = forms.DateTimeField(required=False, input_formats=ISO8601_FORMAT)
+    before = IsoDateTimeField(required=False)
+    after = IsoDateTimeField(required=False)
     limit = forms.IntegerField(required=False)
