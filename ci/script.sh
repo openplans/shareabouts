163dd69a8941526a0a0a5cf6c2fbfc00d6691d44
Aaron Ogle <atogle@gmail.com>
2012-07-27 18:12:09 -0400

    debugging travis test script

--- ci/script.sh
+++ ci/script.sh
@@ -1,10 +1,9 @@
-pwd
-
 # Run the python tests
 src/manage.py test sa_web
 STATUS=$?
 if [ $STATUS -ne 0 ]
 then exit $STATUS
+fi
 
 # Change to the jasmine directory and run the JS tests
 cd src/sa_web/jasmine/
@@ -12,4 +11,5 @@ bundle exec rake jasmine:ci
 STATUS=$?
 if [ $STATUS -ne 0 ]
 then exit $STATUS
+fi
 cd ../../..
