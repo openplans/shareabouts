a7dc7cee615a29ab387c509c7e7d884c65dd0cb5
Aaron Ogle <atogle@gmail.com>
2012-07-30 08:49:34 -0400

    add asset compression

--- src/project/settings.py
+++ src/project/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = './'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -81,6 +81,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -129,6 +130,7 @@ INSTALLED_APPS = (
     # 3rd-party reusaple apps
     'south',
     'mustachejs',
+    'compressor',
 
     # Project apps
     'sa_web',
