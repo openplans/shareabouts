f5e4e069ac8bc56e7e2ed7fbaf0a414540ae4fe8
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-08-03 15:31:07 -0400

    Update tests to reflect class-based-ness of views

--- src/sa_manager/tests.py
+++ src/sa_manager/tests.py
@@ -6,78 +6,81 @@ The process_new_attr function...
 
 from django.test import TestCase
 from nose.tools import *
-from .views import process_new_attr
+from .views import BaseDataBlobFormMixin
 
 
 class TestProcessNewAttrFunction(TestCase):
+    def setUp(self):
+        self.view = BaseDataBlobFormMixin()
+
     @istest
     def should_return_empty_key_and_value_for_empty_dict(self):
-        data = {}
+        data = self.view.data_blob = {}
         num = 1
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('', ''))
         assert_equal(data, {})
 
     @istest
     def should_return_empty_key_and_value_for_unused_new_attr_num(self):
-        data = {'_new_key2': 'attr', '_new_val2': 'value'}
+        data = self.view.data_blob = {'_new_key2': 'attr', '_new_val2': 'value'}
         num = 1
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('', ''))
         assert_equal(data, {'_new_key2': 'attr', '_new_val2': 'value'})
 
     @istest
     def should_return_the_key_and_value_for_the_new_attr_num(self):
-        data = {'_new_key2': 'attr', '_new_val2': 'value'}
+        data = self.view.data_blob = {'_new_key2': 'attr', '_new_val2': 'value'}
         num = 2
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('attr', 'value'))
         assert_equal(data, {'attr': 'value'})
 
     @istest
     def should_return_the_key_and_empty_value_for_the_new_attr_num(self):
-        data = {'_new_val2': 'value'}
+        data = self.view.data_blob = {'_new_val2': 'value'}
         num = 2
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('', 'value'))
         assert_equal(data, {})
 
     @istest
     def should_return_empty_key_and_the_value_for_the_new_attr_num(self):
-        data = {'_new_key2': 'attr'}
+        data = self.view.data_blob = {'_new_key2': 'attr'}
         num = 2
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('attr', ''))
         assert_equal(data, {})
 
     @istest
     def should_return_empty_for_a_whitespace_key(self):
-        data = {'_new_key2': ' \t\n', '_new_val2': 'value'}
+        data = self.view.data_blob = {'_new_key2': ' \t\n', '_new_val2': 'value'}
         num = 2
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('', 'value'))
         assert_equal(data, {})
 
     @istest
     def should_trim_key(self):
-        data = {'_new_key2': ' attr\n', '_new_val2': 'value'}
+        data = self.view.data_blob = {'_new_key2': ' attr\n', '_new_val2': 'value'}
         num = 2
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('attr', 'value'))
         assert_equal(data, {'attr': 'value'})
 
     @istest
     def should_return_the_key_and_value_for_a_string_new_attr_num(self):
-        data = {'_new_key2': 'attr', '_new_val2': 'value'}
+        data = self.view.data_blob = {'_new_key2': 'attr', '_new_val2': 'value'}
         num = '2'
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('attr', 'value'))
         assert_equal(data, {'attr': 'value'})
 
     @istest
     def should_remove_the_key_and_value_from_the_dict(self):
-        data = {'_new_key2': 'attr', '_new_val2': 'value', 'otherattr': 5}
+        data = self.view.data_blob = {'_new_key2': 'attr', '_new_val2': 'value', 'otherattr': 5}
         num = '2'
-        key, val = process_new_attr(data, num)
+        key, val = self.view.process_new_attr(num)
         assert_equal((key, val), ('attr', 'value'))
         assert_equal(data, {'attr': 'value', 'otherattr': 5})
