5fbf149a8836ea9580692075c7b45230db0d23af
Paul Winkler <slinkp@gmail.com>
2012-08-17 16:35:47 -0400

    Add support for api keys in the admin UI.  Also, whitespace.

--- src/sa_api/admin.py
+++ src/sa_api/admin.py
@@ -5,26 +5,38 @@ via django.contrib.admin.
 
 import models
 from django.contrib import admin
+from .apikey.models import ApiKey
+
 
 class SubmittedThingAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_datetime'
     model = models.SubmittedThing
     list_display = ('id', 'created_datetime', 'updated_datetime', 'submitter_name',)
 
+
+class InlineApiKeyAdmin(admin.StackedInline):
+    model = ApiKey.datasets.through
+
+
 class DataSetAdmin(admin.ModelAdmin):
     list_display = ('id', 'owner', 'short_name', 'display_name')
     prepopulated_fields = {'short_name': ['display_name']}
+    inlines = [InlineApiKeyAdmin]
+
 
 class PlaceAdmin(SubmittedThingAdmin):
     list_display = SubmittedThingAdmin.list_display + ('dataset', )
     model = models.Place
 
+
 class SubmissionSetAdmin(admin.ModelAdmin):
     list_display = ('id', 'submission_type',)
     list_filter = ('submission_type',)
 
+
 class SubmissionAdmin(SubmittedThingAdmin):
     model = models.Submission
+
 
 class ActivityAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_datetime'
