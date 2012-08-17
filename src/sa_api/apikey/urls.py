178652fed0c9ea8f5861c33aa84cc719c8033220
Paul Winkler <slinkp@gmail.com>
2012-08-17 16:40:59 -0400

    Remove stuff we probably don't need from apikey.

--- src/sa_api/apikey/urls.py
+++ src/sa_api/apikey/urls.py
@@ -9,11 +9,11 @@ http://pypi.python.org/pypi/django-apikey
 from django.conf.urls.defaults import *
 
 
-urlpatterns = patterns('ebpub.openblockapi.apikey.views',
-                       url(r'^create_key/$', 'generate_key', 
-                           name='api_create_key' ),
-                       url(r'^keys/$', 'list_keys',
-                           name='api_list_keys' ),
-                       url(r'^delete_key/$', 'delete_key',
-                           name='api_delete_key' ),
-                       )
+# urlpatterns = patterns('sa_api.apikey.views',
+#                        url(r'^create_key/$', 'generate_key', 
+#                            name='api_create_key' ),
+#                        url(r'^keys/$', 'list_keys',
+#                            name='api_list_keys' ),
+#                        url(r'^delete_key/$', 'delete_key',
+#                            name='api_delete_key' ),
+#                        )
