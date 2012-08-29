67a00ef79ffc8dbd32e4d2f59cf405c650a4ca97
Paul Winkler <slinkp@gmail.com>
2012-08-29 11:36:08 -0400

    Tests of DjangoRestFramework-compatible auth class

--- src/sa_api/apikey/tests.py
+++ src/sa_api/apikey/tests.py
@@ -7,24 +7,29 @@ class TestApiKeyAuth(unittest2.TestCase):
 
     def test_check_api_auth__no_credentials(self):
         from .auth import check_api_authorization
+        from .auth import ApiKeyAuthentication
         ip = '1.2.3.4'
         request = mock.Mock(**{'user.is_authenticated.return_value': False,
                                'META': {'REMOTE_ADDR': ip},
                                'GET': {}, 'POST': {}})
         self.assertRaises(PermissionDenied, check_api_authorization,
                           request)
-
+        self.assertEqual(None, ApiKeyAuthentication(None).authenticate(request))
 
     def test_check_api_auth__logged_in(self):
         from .auth import check_api_authorization
+        from .auth import ApiKeyAuthentication
         ip = '1.2.3.4'
         request = mock.Mock(**{'user.is_authenticated.return_value': True,
                                'META': {'REMOTE_ADDR': ip},
                                'GET': {}, 'POST': {}})
         self.assertEqual(True, check_api_authorization(request))
+        self.assertEqual(request.user,
+                         ApiKeyAuthentication(None).authenticate(request))
 
     def test_check_api_auth__key_invalid(self):
         from .auth import check_api_authorization, KEY_HEADER
+        from .auth import ApiKeyAuthentication
         from django.core.exceptions import PermissionDenied
         key = '12345'
         ip = '1.2.3.4'
@@ -34,9 +39,12 @@ class TestApiKeyAuth(unittest2.TestCase):
                                    'GET': {}, 'POST': {}})
         self.assertRaises(PermissionDenied, check_api_authorization,
                           get_request)
+        self.assertEqual(None,
+                         ApiKeyAuthentication(None).authenticate(get_request))
 
     def test_check_api_auth__key(self):
         from .auth import check_api_authorization, KEY_HEADER
+        from .auth import ApiKeyAuthentication
         from .models import generate_unique_api_key
         from .models import ApiKey
         from django.contrib.auth.models import User
@@ -51,3 +59,5 @@ class TestApiKeyAuth(unittest2.TestCase):
                                    'session': mock.MagicMock(),
                                    'GET': {}, 'POST': {}})
         self.assertEqual(True, check_api_authorization(get_request))
+        self.assertEqual(get_request.user,
+                         ApiKeyAuthentication(None).authenticate(get_request))
