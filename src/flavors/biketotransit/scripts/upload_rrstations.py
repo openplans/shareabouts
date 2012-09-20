7f7e3629fc0172768609a45a960540536e1974de
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-09-20 15:03:21 -0400

    Add flavor for biketotransit

---
+++ src/flavors/biketotransit/scripts/upload_rrstations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import csv
+import logging
+import shareabouts
+import sys
+
+logging.basicConfig(level='DEBUG')
+
+def main(api_key):
+    api = shareabouts.ShareaboutsApi('http://shareaboutsapi-civicworks.dotcloud.com/api/v1/')
+    api.authenticate_with_key(api_key)
+    dataset = 'biketotransit'
+    owner = 'biketotransit'
+
+    railstations_url = api.build_uri('place_collection', username=owner,
+                                     dataset_slug=dataset)
+
+    with open('rail-stations.csv') as railstationsfile:
+        headers = None
+        reader = csv.reader(railstationsfile)
+        for row in reader:
+            if headers is None:
+                headers = [header.lower() for header in row]
+                continue
+
+            station = dict(zip(headers, row))
+            station['location'] = {
+                'lat': float(station.pop('lat')),
+                'lng': float(station.pop('long'))
+            }
+            station['name'] = station.pop('label')
+            station['location_type'] = station.pop('type')
+            station['visible'] = True
+
+            response = api.send('POST', url=railstations_url, data=station)
+
+
+if __name__ == '__main__':
+    api_key = sys.argv[1]
+    sys.exit(main(api_key))
