4f8980bde7c503e411463452fbd51f32565e1dc1
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-09-27 18:20:30 -0400

    Add the staticfiles urls to the URL patterns.
    These patterns should be disregarded if your server is configured to serve static files from elsewhere.

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+# By default, static assets will be served from Django.  It is recommended that
+# you use a better suited server instead.  Consult the documentation on serving
+# static files with Django for your deploy platform.
+urlpatterns = staticfiles_urlpatterns() + patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^project/', include('project.foo.urls')),
