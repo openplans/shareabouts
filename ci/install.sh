6feda1c5b1b41df179babe726713e5c9a647e796
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-10-08 16:43:19 -0400

    Update the travis setup script to not do database stuff

--- ci/install.sh
+++ ci/install.sh
@@ -3,34 +3,8 @@
 # libevent development files are required for gevent
 sudo apt-get install libevent-dev
 
-# Install GeoDjango dependencies -- see
-# https://docs.djangoproject.com/en/dev/ref/contrib/gis/install/#ubuntu
-sudo apt-get install binutils gdal-bin libproj-dev postgresql-9.1-postgis \
-     postgresql-server-dev-9.1 python-psycopg2
-
 # Install the python requirements
 sudo pip install -r requirements.txt
-
-# Create a PostGIS template database
-psql -c "CREATE DATABASE template_postgis;" -U postgres
-psql -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';" -U postgres
-createlang plpgsql template_postgis -U postgres
-# Loading the PostGIS SQL routines
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql -q
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql -q
-# Enabling users to alter spatial tables.
-psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
-psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
-psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
-
-# Initialize the database
-psql -U postgres <<EOF
-    CREATE USER shareabouts WITH PASSWORD 'shareabouts';
-    CREATE DATABASE shareabouts_v1 WITH TEMPLATE = template_postgis;
-    GRANT ALL ON DATABASE template_postgis TO shareabouts;
-    GRANT ALL ON DATABASE shareabouts_v1 TO shareabouts;
-    ALTER USER shareabouts WITH CREATEDB;
-EOF
 
 # Initialize the project settings
 cp src/project/local_settings.py.template src/project/local_settings.py
