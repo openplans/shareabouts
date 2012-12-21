8794f6fb8a1024f310e26abfb77bc1e8e0d5b1dc
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2012-12-21 15:49:03 -0500

    Add create and update methods to the client module

--- src/flavors/biketotransit/scripts/shareabouts.py
+++ src/flavors/biketotransit/scripts/shareabouts.py
@@ -8,6 +8,7 @@ class ShareaboutsApi (object):
         'keys_collection': r'datasets/{username}/{dataset_slug}/keys/',
         'place_collection': r'datasets/{username}/{dataset_slug}/places/',
         'place_instance': r'datasets/{username}/{dataset_slug}/places/{pk}/',
+        'all_submissions': r'datasets/{username}/{dataset_slug}/{type}/',
         'submission_collection': r'datasets/{username}/{dataset_slug}/places/{place_pk}/{type}/',
         'submission_instance': r'datasets/{username}/{dataset_slug}/places/{place_pk}/{type}/{pk}/',
     }
@@ -34,12 +35,13 @@ class ShareaboutsApi (object):
     def authenticate_with_key(self, key):
         self.key = key
 
-    def send(self, method, url, data=None):
+    def send(self, method, url, data=None, silent=False):
         if data is not None:
             data = json.dumps(data)
 
         headers = {'Content-type': 'application/json',
-                   'Accept': 'application/json'}
+                   'Accept': 'application/json',
+                   'X-Shareabouts-Silent': str(silent)}
 
         # Set authentication headers
         if hasattr(self, 'csrf_token') and hasattr(self, 'cookies'):
@@ -52,7 +54,7 @@ class ShareaboutsApi (object):
             headers.update({
                 'X-Shareabouts-Key': self.key
             })
-
+        
         # Explicitly set the content length for delete
         if method == 'DELETE':
             headers.update({'Content-Length': '0'})
@@ -60,11 +62,62 @@ class ShareaboutsApi (object):
         response = requests.request(method, url, data=data, headers=headers)
         return response
 
-    def get(self, url, default=None):
+    def get(self, url, default=None, silent=False):
         """
         Returns decoded data from a GET request, or default on non-200
         responses.
         """
-        res = self.send('GET', url)
-        res_json = res.text
-        return (json.loads(res_json) if res.status_code == 200 else default)
+        response = self.send('GET', url, silent=silent)
+        
+        if res.status_code != 200:
+            return default
+        
+        data = self.parse(response.text)
+        return data
+    
+    def parse(self, response_text):
+        data = json.loads(response_text)
+        # TODO: Raise appropriate exception on invalid JSON data
+        return data
+
+    #
+    # CRUD
+    def retrieve(self, url):
+        """
+        Returns decoded data from a GET request, or default on non-200
+        responses.
+        """
+        response = self.send('GET', url, silent=silent)
+        
+        if res.status_code != 200:
+            raise Exception()
+            # TODO: raise appropriate exception
+        
+        data = self.parse(response.text)
+        return data
+    
+    def update(self, instance, url=None, silent=False):
+        if url is None:
+            url = instance['url']
+            # TODO: Raise specific error on KeyError
+        
+        data = instance.copy()
+        
+        # Reserved Attributes
+        # -------------------
+        # For everything:
+        data.pop('created_datetime')
+        data.pop('updated_datetime')
+        data.pop('id')
+        # For submitted things:
+        data.pop('dataset', None)
+        data.pop('submissions', None)
+        
+        response = self.send('PUT', url, data, silent)
+        data = self.parse(response.text)
+        return data
+    
+    def create(self, collection_url, data, silent=False):
+        response = self.send('POST', collection_url, data, silent)
+        new_data = self.parse(response.text)
+        return new_data
