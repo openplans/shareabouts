0522129cc23894a2666fa8649a1cb2338bc5cf34
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-01-18 17:11:54 -0500

    Add link to place detail page from place summary list

--- src/flavors/discussions/static/js/place-summary-list-view.js
+++ src/flavors/discussions/static/js/place-summary-list-view.js
@@ -22,11 +22,13 @@ var Shareabouts = Shareabouts || {};
         return S.Util.getPrettyDateTime(data.created_datetime,
           self.options.placeConfig.pretty_datetime_format);
       };
+
+      data.link_path = '/place/' + data.id;
+
       responseData = _.find(data.submissions, function(sset) {
         return sset.type === self.options.surveyConfig.submission_type;
       });
       data.responses = responseData || {length: 0};
-
       // The template will need to know whether to use singular or plural
       data.has_single_response = (data.responses.length === 1);
 
