3cbf3a73dff1517f5e06098325162ae6b8ae7473
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-12-03 16:59:26 -0500

    Add a context processor that makes the settings available in templates

---
+++ src/project/context_processors.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+def settings_context(request):
+    return {'settings': settings}
--- src/project/settings.py
+++ src/project/settings.py
@@ -89,6 +89,17 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+
+    "project.context_processors.settings_context",
+)
+
 MIDDLEWARE_CLASSES = (
     'sa_web.middleware.CacheRequestBody',
     'django.contrib.sessions.middleware.SessionMiddleware',
