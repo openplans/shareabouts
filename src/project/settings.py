2548456a14c46a8026112ff246f5ff5f8a125930
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-12-23 20:50:41 -0500

    Add an allowed_hosts setting for Django 1.5+

--- src/project/settings.py
+++ src/project/settings.py
@@ -18,6 +18,8 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ['*']
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
