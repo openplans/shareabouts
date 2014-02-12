33d3d25b42d08f0d1efdbc80c1d46ea23cfd2532
Aaron Ogle <atogle@gmail.com>
2014-02-12 13:14:05 -0500

    Support place list filtering - Bind input and submit events on the search form - Trigger show/hide events on the place model - Bind show/hide events on the place list item view

--- src/sa_web/static/js/views/place-list-view.js
+++ src/sa_web/static/js/views/place-list-view.js
@@ -23,6 +23,9 @@ var Shareabouts = Shareabouts || {};
           submissionType: supportType,
           placeModel: this.model
         });
+
+      this.listenTo(this.model, 'show', this.show);
+      this.listenTo(this.model, 'hide', this.hide);
     },
     onRender: function(evt) {
       this.support.show(new S.SupportView({
@@ -30,13 +33,58 @@ var Shareabouts = Shareabouts || {};
         supportConfig: S.Config.support,
         userToken: S.Config.userToken
       }));
+    },
+    show: function() {
+      this.$el.show();
+    },
+    hide: function() {
+      this.$el.hide();
     }
   });
 
   S.PlaceListView = Backbone.Marionette.CompositeView.extend({
     template: '#place-list',
     itemView: S.PlaceListItemView,
-    itemViewContainer: '.place-list'
+    itemViewContainer: '.place-list',
+    ui: {
+      searchField: '#list-search',
+      searchForm: '.list-search-form'
+    },
+    events: {
+      'input @ui.searchField': 'handleSearchInput',
+      'submit @ui.searchForm': 'handleSearchSubmit'
+    },
+    handleSearchInput: function(evt) {
+      evt.preventDefault();
+      this.filter(this.ui.searchField.val());
+    },
+    handleSearchSubmit: function(evt) {
+      evt.preventDefault();
+      this.filter(this.ui.searchField.val());
+    },
+    filter: function(term) {
+      var len = S.Config.place.items.length,
+          val, key, i;
+
+      term = term.toUpperCase();
+      this.collection.each(function(model) {
+        var show = false;
+        for (i=0; i<len; i++) {
+          key = S.Config.place.items[i].name;
+          val = model.get(key);
+          if (_.isString(val) && val.toUpperCase().indexOf(term) !== -1) {
+            show = true;
+            break;
+          }
+        }
+
+        if (show) {
+          model.trigger('show');
+        } else {
+          model.trigger('hide');
+        }
+      });
+    }
   });
 
 }(Shareabouts, jQuery, Shareabouts.Util.console));
