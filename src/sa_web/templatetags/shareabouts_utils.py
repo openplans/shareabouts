d4ba42176fcb4791d69b77091d542f85f6942ab7
Aaron Ogle <atogle@gmail.com>
2014-04-30 11:53:15 -0400

    Add get_item template filter for accessing dictionary values

--- src/sa_web/templatetags/shareabouts_utils.py
+++ src/sa_web/templatetags/shareabouts_utils.py
@@ -4,6 +4,12 @@ from django.utils.safestring import mark_safe
 
 register = Library()
 
+
 @register.filter
 def as_json(data):
     return mark_safe(json.dumps(data))
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
