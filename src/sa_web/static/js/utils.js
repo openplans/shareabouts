796860a918071b72495eb66ca17242a165824f08
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-06-26 12:49:03 -0400

    Don't send the jquery cachebusting parameter to mapquest

--- src/sa_web/static/js/utils.js
+++ src/sa_web/static/js/utils.js
@@ -311,6 +311,7 @@ var Shareabouts = Shareabouts || {};
 
         options = options || {};
         options.dataType = 'jsonp';
+        options.cache = true;
         options.url = 'http://open.mapquestapi.com/geocoding/v1/address?key=' + mapQuestKey + '&location=' + location;
         if (bounds) {
           options.url += '&boundingBox=' + bounds.join(',');
