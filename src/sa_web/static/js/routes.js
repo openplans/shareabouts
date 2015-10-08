7ed0b8047aaee07f9b34fbbad755d5f081bc656a
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-10-08 12:40:48 -0400

    Add existence checking before working with the filter menu

--- src/sa_web/static/js/routes.js
+++ src/sa_web/static/js/routes.js
@@ -153,8 +153,13 @@ var Shareabouts = Shareabouts || {};
       }
 
       // Get the menu information for the current location type
-      var filterMenu = _.findWhere(S.Config.pages, {'slug': 'filter-type'});
-      var menuItem = _.findWhere(filterMenu.pages, {'url': '/filter/' + locationType});
+      var filterMenu, menuItem;
+      if (S.Config.pages) {
+        filterMenu = _.findWhere(S.Config.pages, {'slug': 'filter-type'});
+      }
+      if (filterMenu) {
+        menuItem = _.findWhere(filterMenu.pages, {'url': '/filter/' + locationType});
+      }
 
       if (locationType !== 'all') {
         this.appView.mapView.filter(locationType);
