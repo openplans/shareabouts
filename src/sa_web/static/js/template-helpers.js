47cf53a90aa1a497baa228c01d963b9195a26fda
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2017-02-26 22:19:01 -0500

    Add widget types for radio and checkbox groups. Add default styles as toggle buttons

--- src/sa_web/static/js/template-helpers.js
+++ src/sa_web/static/js/template-helpers.js
@@ -5,9 +5,16 @@ var Shareabouts = Shareabouts || {};
     // Attached helper properties for how to display this form element
     insertInputTypeFlags: function(configItems) {
       _.each(configItems, function(item, index) {
-        item.is_input = (!item.type || (item.type !== 'textarea' && item.type !== 'select' && item.type !== 'file'));
+        item.is_input = (!item.type || (
+                            item.type !== 'textarea' &&
+                            item.type !== 'select' &&
+                            item.type !== 'radiogroup' &&
+                            item.type !== 'checkboxgroup' &&
+                            item.type !== 'file'));
         item.is_textarea = (item.type === 'textarea');
         item.is_select = (item.type === 'select');
+        item.is_radiogroup = (item.type === 'radiogroup');
+        item.is_checkboxgroup = (item.type === 'checkboxgroup');
         item.is_file = (item.type === 'file');
         item.is_fileinput_supported = S.Util.fileInputSupported();
       });
