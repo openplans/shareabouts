db2742be4c2ddf6e47c22d931c5624f5c26d007a
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2017-08-07 18:32:36 -0400

    Update flavormessages for Python3 and Django1.8+

--- src/sa_web/management/commands/flavormessages.py
+++ src/sa_web/management/commands/flavormessages.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from django.core.management import call_command
 from django.core.management.commands.makemessages import Command as MakeMessagesCommand
 import os
@@ -5,6 +7,9 @@ import os.path
 
 from sa_web.config import get_shareabouts_config
 from django.conf import settings
+
+try: str_type = basestring  # Python 2
+except: str_type = str      # Python 3
 
 
 def spit_to_file(data, outfile, exclude=[]):
@@ -19,7 +24,7 @@ def spit_to_file(data, outfile, exclude=[]):
             spit_to_file(item, outfile)
 
     # If it's a string, output it, unless it should be excluded
-    elif isinstance(data, basestring):
+    elif isinstance(data, str_type):
         msg = parse_msg(data)
         if msg is not None:
             outfile.write('_(r"""' + msg + '""")\n')
@@ -31,15 +36,15 @@ def parse_msg(s):
 
 
 class Command(MakeMessagesCommand):
-    def handle_noargs(self, *args, **options):
+    def handle(self, *args, **options):
         # Load the config file
-        print "Loading config file from", settings.SHAREABOUTS.get('CONFIG')
+        print("Loading config file from " + settings.SHAREABOUTS.get('CONFIG'))
         config = get_shareabouts_config(settings.SHAREABOUTS.get('CONFIG'))
         config.raw = True  # So that we don't preprocess through the translator
 
         # Generate an intermediary Python file
         mfile_path = os.path.join(config.path, '_config.translations.py')
-        print "Writing intermediary file", mfile_path
+        print("Writing intermediary file" + mfile_path)
         with open(mfile_path, 'w') as mfile:
             spit_to_file(config.data, mfile)
 
