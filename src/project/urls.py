38601c42e71dd6d66d6c5d9f05281b02cf7eeac2
Mjumbe Poe <mjumbewu@gmail.com>
2023-01-09 01:54:55 -0500

    Update URL patterns in prep for Django 1.10

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.views.i18n import set_language
 
 admin.autodiscover()
 
@@ -12,7 +13,7 @@ admin.autodiscover()
 # you use a better suited server instead.  Consult the documentation on serving
 # static files with Django for your deploy platform.
 
-urlpatterns = staticfiles_urlpatterns() + patterns('',
+urlpatterns = staticfiles_urlpatterns() + [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^project/', include('project.foo.urls')),
@@ -24,12 +25,12 @@ urlpatterns = staticfiles_urlpatterns() + patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^choose-language$', 'django.views.i18n.set_language', name='set_language'),
+    url(r'^choose-language$', set_language, name='set_language'),
     url(r'^', include('sa_web.urls')),
-)
+]
 
 from django.conf import settings
 if settings.SHAREABOUTS['DATASET_ROOT'].startswith('/'):
-    urlpatterns = patterns('',
+    urlpatterns = [
         url(r'^full-api/', include('sa_api_v2.urls')),
-    ) + urlpatterns
+    ] + urlpatterns
