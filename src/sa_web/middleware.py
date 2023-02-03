e6471d3a1f66c8e797de0687426565a2c8edefa2
Mjumbe Poe <mjumbewu@gmail.com>
2023-02-03 17:20:06 -0500

    Changes for Django 2.2 upgrade
    - New TEMPLATES settings variable
    - New MIDDLEWARE definitions
    - Remove "load url from future" in templates

--- src/sa_web/middleware.py
+++ src/sa_web/middleware.py
@@ -3,8 +3,17 @@ Middleware to come before the CsrfViewMiddleware so that the request body can
 be accessed even after a POST request with multipart data.
 """
 
-class CacheRequestBody (object):
-    def process_request(self, request):
+
+class CacheRequestBody:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         # Call request.body, so that the request remembers the contents of the
-        # body.
-        request.body
+        # body. This is necessary because the CsrfViewMiddleware will read the
+        # body, and then the request will be unable to read it again.
+        #
+        # (This line is thus not pointless, even though pylint thinks it is)
+        request.body  # pylint: disable=pointless-statement
+        response = self.get_response(request)
+        return response
