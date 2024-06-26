2fdad5d4b11a91e17aa93d7c960821378daf5eb8
Ben Sturmfels <ben@sturm.com.au>
2024-06-26 14:38:18 +1000

    Switch to Django 2.0-style path() URLconfs
    Fixes RemovedInDjango40Warning.
    
    Removed the commented out examples from the Django default template, since
    these aren't really relevant.
    
    Also removed `staticfiles_urlpatterns` as these are provided automatically by
    Django in development and are disabled in production.

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,36 +1,17 @@
-from django.conf.urls import include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
+from django.conf import settings
 from django.contrib import admin
 from django.views.i18n import set_language
 
 admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-# By default, static assets will be served from Django.  It is recommended that
-# you use a better suited server instead.  Consult the documentation on serving
-# static files with Django for your deploy platform.
-
-urlpatterns = staticfiles_urlpatterns() + [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^project/', include('project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^admin/', admin.site.urls),
-    url(r'^choose-language$', set_language, name='set_language'),
-    url(r'^', include('sa_web.urls')),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('choose-language', set_language, name='set_language'),
+    path('', include('sa_web.urls')),
 ]
 
-from django.conf import settings
 if settings.SHAREABOUTS['DATASET_ROOT'].startswith('/'):
     urlpatterns = [
-        url(r'^full-api/', include('sa_api_v2.urls')),
+        path('full-api/', include('sa_api_v2.urls')),
     ] + urlpatterns
