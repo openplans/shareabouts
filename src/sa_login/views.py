c5333bff6c5661d91ab5831132a4218bfe4d4ba1
Mjumbe Poe <mjumbewu@gmail.com>
2024-09-26 11:42:34 -0400

    Add some console messages to the authentication tree

--- src/sa_login/views.py
+++ src/sa_login/views.py
@@ -19,24 +19,31 @@ def login(request: HttpRequest):
 
     # POST a new user session to log in to the API
     elif request.method == 'POST' and request.POST.get('shadowmethod', '').upper() != 'DELETE':
+        print('Logging in...')
         try:
             api.login(request.POST.get('username'), request.POST.get('password'))
             api_user = api.current_user()
+            print('Successfully logged in to the API session.')
         except ShareaboutsApiError as exc:
             error_str = f'Login failed. {"; ".join(exc.errors.values()) if exc.errors else "Please try again."}'
+            print('Failed to log in to the API session:', exc)
         next_url = request.POST.get('next', None)
 
     # DELETE the current user session to log out of the API
     elif request.method == 'POST' and request.POST.get('shadowmethod', '').upper() == 'DELETE':
+        print('Logging out...')
         try:
             api.logout()
             api_user = api.current_user()
-        except ShareaboutsApiError:
+            print('Successfully logged out of the API session.')
+        except ShareaboutsApiError as exc:
             error_str = 'Failed to log out. Please try again.'
+            print('Failed to log out of the API session:', exc)
         next_url = request.POST.get('next', None)
 
     if api_user and next_url:
         response = redirect(next_url)
+        print(f'Redirecting to {next_url}...')
     else:
         response = render(request, 'sa_login.html', {
             'api_user': api_user,
@@ -46,5 +53,6 @@ def login(request: HttpRequest):
             'auth_root': api.auth_root,
             'next_url': next_url,
         })
+        print('Rendering the login page...')
 
     return api.respond_with_session_cookie(response)
